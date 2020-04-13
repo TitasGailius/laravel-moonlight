a5a10a5bf05e643e11632a399e9c44009037673d
Gilbert Pellegrom <gilbert@pellegrom.me>
2020-04-13 17:30:15 +0100

    Use app name as title in app.blade.php (#3)

--- src/moonlight-stubs/resources/views/app.blade.php
+++ src/moonlight-stubs/resources/views/app.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-        <title>Laravel</title>
+        <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,400,600" rel="stylesheet">
