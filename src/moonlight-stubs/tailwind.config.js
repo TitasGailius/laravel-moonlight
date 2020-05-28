64ed82a23d8eee0b758e1738258241c659570ab0
Elliot <elliot@raison.co>
2020-05-28 12:33:51 +0100

    adds new purgecss paths to the tailwind config file (#9)

--- src/moonlight-stubs/tailwind.config.js
+++ src/moonlight-stubs/tailwind.config.js
@@ -1,15 +1,33 @@
 module.exports = {
-    theme: {
-        extend: {
-            screens: {
-                'xl': '1140px',
-            }
-        },
-        container: {
-            center: true,
-            padding: '1rem',
-        }
+  purge: {
+    // enabled: true,
+    content: [
+      "app/**/*.php",
+      "resources/**/*.html",
+      "resources/**/*.js",
+      "resources/**/*.jsx",
+      "resources/**/*.ts",
+      "resources/**/*.tsx",
+      "resources/**/*.php",
+      "resources/**/*.vue",
+      "resources/**/*.twig",
+    ],
+    options: {
+      // defaultExtractor: (content) => content.match(/[\w-/.:]+(?<!:)/g) || [],
+      whitelistPatterns: [/-active$/, /-enter$/, /-leave-to$/, /show$/],
     },
-    variants: {},
-    plugins: [],
-}
+  },
+  theme: {
+    extend: {
+      screens: {
+        xl: "1140px",
+      },
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+  },
+  variants: {},
+  plugins: [],
+};
